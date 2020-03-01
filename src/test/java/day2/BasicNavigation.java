eb6781d15944d70e1cdd1900a217a1e41fc14fb8
sadrettincali <sadrettincali@gmail.com>
2020-03-01 13:30:00 -0500

    new

---
+++ src/test/java/day2/BasicNavigation.java
@@ -0,0 +1,16 @@
+package day2;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BasicNavigation {
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("http://google.com");
+
+
+
+    }
+}
