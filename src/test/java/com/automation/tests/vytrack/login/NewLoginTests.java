275d14ba09cb0271bb10376dce118b51b8224362
sadrettincali <sadrettincali@gmail.com>
2020-04-04 12:01:52 -0400

    new

--- src/test/java/com/automation/tests/vytrack/login/NewLoginTests.java
+++ src/test/java/com/automation/tests/vytrack/login/NewLoginTests.java
@@ -5,35 +5,59 @@ import com.automation.tests.vytrack.AbstractTestBase;
 import com.automation.utilities.BrowserUtils;
 import com.automation.utilities.Driver;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class NewLoginTests extends AbstractTestBase {
 
 
     @Test
-    public void verifyPageTitle(){
+    public void verifyPageTitle() {
         //test --> ExtentTest object
         //we must add to every test at the beginning
         //test = report.createTest("Test name");
         test = report.createTest("Verify page title");
         LoginPage loginPage = new LoginPage();
         loginPage.login();
-        Assert.assertEquals(Driver.getDriver().getTitle(), "Dashboar");
+        Assert.assertEquals(Driver.getDriver().getTitle(), "Dashboard");
         //if assertion passed, it will set test status in report to passed
         test.pass("Page title Dashboard was verified");
     }
 
     /**
      * Enter wrong credentials and verify warning message
-     *
      */
 
     @Test
-    public void verifyWarningMessage(){
+    public void verifyWarningMessage() {
         LoginPage loginPage = new LoginPage();
         loginPage.login("wrong", "wrong");
         Assert.assertEquals(loginPage.getWarningMessageText(), "Invalid user name or password.");
         //take a screenshot
         BrowserUtils.getScreenshot("warning_message");
     }
+
+    @Test(dataProvider = "credentials")
+    public void loginWithDDT(String userName, String password) {
+        test = report.createTest("Verify page title");
+        LoginPage loginPage = new LoginPage();
+        loginPage.login(userName, password);
+        test.info("Login as " + userName);//log some steps
+        BrowserUtils.wait(2);
+        Assert.assertEquals(Driver.getDriver().getTitle(), "Dashboard");
+        test.pass("Page title Dashboard was verified");
+    }
+
+    //Object[][] or Object[] or Iterator<Object[]>
+//Object[] - 1 column with a data
+  //Object[][] 2+
+    @DataProvider
+    public Object[][] credentials() {
+        return new Object[][]{
+                {"storemanager85",  "UserUser123"},
+                {"salesmanager110", "UserUser123"},
+                {"user16",          "UserUser123"}
+            };
+    }
 }
+
